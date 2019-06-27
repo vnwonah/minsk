6ee9ed48a99acfbf465975a2d5630c73bbdb75ee
Vincent Nwonah <vnwonah@outlook.com>
2019-06-27 09:32:52 +0100

    added lexer that can generate number, whitespace and operator tokens

---
+++ mc/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace mc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+                if(string.IsNullOrWhiteSpace(line))
+                    return;
+                
+                var lexer = new Lexer(line);
+                while(true)
+                {
+                    var token = lexer.NextToken();
+
+                    if(token.Kind == SyntaxKind.EndOfFileToken)
+                        break;
+                    
+                    Console.Write($"{token.Kind}: '{token.Text}'");
+                    if(token.Value != null)
+                        Console.Write($" {token.Value}");
+                    
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+
+    enum SyntaxKind
+    {
+        NumberToken,
+        BadToken,
+        WhitespaceToken,
+        PlusToken,
+        MinusToken,
+        StarToken,
+        SlashToken,
+        OpenParenthesisToken,
+        CloseParenthesisToken,
+        EndOfFileToken
+    }
+    class SyntaxToken
+    {
+        public SyntaxKind Kind {get;}
+        public int Position {get;}
+        public string Text {get;}
+        public object Value {get;}
+        public SyntaxToken (SyntaxKind kind, int position, string text, object value)
+        {
+            Kind = kind;
+            Position = position;
+            Text = text;
+            Value = value;
+        }
+    }
+
+    class Lexer
+    {
+        private readonly string _text;
+        private int _position;
+
+        private char Current
+        {
+            get
+            {
+                if(_position >= _text.Length)
+                    return '\0';
+
+                return _text[_position];
+            }
+        }
+
+        private void Next() => _position++;
+        public Lexer(string text)
+        {
+            _text = text;
+        }
+
+        public SyntaxToken NextToken()
+        {
+            if(_position >= _text.Length)
+                return new SyntaxToken(SyntaxKind.EndOfFileToken, _position, "\0", null);
+
+            if(char.IsDigit(Current))
+            {
+                var start = _position;
+
+                while(char.IsDigit(Current))
+                    Next();
+                
+                var length = _position - start;
+                var text = _text.Substring(start, length);
+                int.TryParse(text, out var value);
+
+                return new SyntaxToken(SyntaxKind.NumberToken, start, text, value);
+            }
+
+            if(char.IsWhiteSpace(Current))
+            {
+                var start = _position;
+
+                while(char.IsWhiteSpace(Current))
+                    Next();
+
+                var length = _position - start;
+                var text = _text.Substring(start, length);
+
+                return new SyntaxToken(SyntaxKind.WhitespaceToken, start, text, null);
+            }
+
+            if(Current == '+')
+                return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null);
+            
+            if(Current == '-')
+                return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
+            
+            if(Current == '*')
+                return new SyntaxToken(SyntaxKind.StarToken, _position++, "*", null);
+            
+            if(Current == '/')
+                return new SyntaxToken(SyntaxKind.SlashToken, _position++, "/", null);
+
+            if(Current == '(')
+                return new SyntaxToken(SyntaxKind.OpenParenthesisToken, _position++, "(", null);
+
+            if(Current == ')')
+                return new SyntaxToken(SyntaxKind.CloseParenthesisToken, _position++, ")", null);
+           
+            return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position - 1, 1), null);
+        }
+    }
+}
